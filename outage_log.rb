208a308fdd18c38fece1183770e5fa62e2aa50f6
Matthew M. Boedicker <matthewm@boedicker.org>
2010-01-17 14:34:05 -0500

    handle XONBATT being nil

--- outage_log.rb
+++ outage_log.rb
@@ -14,8 +14,10 @@ LogFile = '/home/mmb/log/power_outage.log'
 FeedFile = '/home/mmb/log/power_outage.atom'
 
 def format_outage(d)
-  last_batt_duration = d['XOFFBATT'] - d['XONBATT']
-  "#{d['XONBATT']}|power outage for #{last_batt_duration} seconds"
+  if d['XOFFBATT'] and d['XONBATT']
+    last_batt_duration = d['XOFFBATT'] - d['XONBATT']
+    "#{d['XONBATT']}|power outage for #{last_batt_duration} seconds"
+  end
 end
 
 time_fields = %w{
@@ -39,44 +41,46 @@ end
 
 last_outage = format_outage(h)
 
-f = open(LogFile, 'a+')
+if last_outage
+  f = open(LogFile, 'a+')
 
-updated = if f.eof? or ((last_line = f.readline.strip) != last_outage)
-  tf = Tempfile.new('outage_rss')
-  tf.write("#{last_outage}\n")
-  tf.write("#{last_line}\n") unless (last_line || '').strip.empty?
-  f.each { |line| tf.write("#{line}") unless line.strip.empty? }
-  f.close
-  tf.close
-  File.copy(tf.path, LogFile)
-  true
-else
-  f.close
-  false
-end
-
-# update feed
-if updated or !File.exists?(FeedFile)
-  feed_lines = []
-  open(LogFile) do |f|
-    while feed_lines.size < 10 and !f.eof?
-      line = f.readline.strip
-      feed_lines.push(line) unless line.empty?
-    end
+  updated = if f.eof? or ((last_line = f.readline.strip) != last_outage)
+    tf = Tempfile.new('outage_rss')
+    tf.write("#{last_outage}\n")
+    tf.write("#{last_line}\n") unless (last_line || '').strip.empty?
+    f.each { |line| tf.write("#{line}") unless line.strip.empty? }
+    f.close
+    tf.close
+    File.copy(tf.path, LogFile)
+    true
+  else
+    f.close
+    false
   end
 
-  feed = Atom::Feed.new do |f|
-    f.title = 'Power Outages'
-    feed_lines.each do |line|
-      time, text = line.split('|')
-      f.entries << Atom::Entry.new do |e|
-        e.title = text
-        e.updated = Time.parse(time)
-        f.updated = [f.updated || Time.at(0), e.updated].max
-        e.summary = text
+  # update feed
+  if updated or !File.exists?(FeedFile)
+    feed_lines = []
+    open(LogFile) do |f|
+      while feed_lines.size < 10 and !f.eof?
+        line = f.readline.strip
+        feed_lines.push(line) unless line.empty?
       end
     end
-  end
 
-  open(FeedFile, 'w') { |f| f.write(feed.to_xml) }
+    feed = Atom::Feed.new do |f|
+      f.title = 'Power Outages'
+      feed_lines.each do |line|
+        time, text = line.split('|')
+        f.entries << Atom::Entry.new do |e|
+          e.title = text
+          e.updated = Time.parse(time)
+          f.updated = [f.updated || Time.at(0), e.updated].max
+          e.summary = text
+        end
+      end
+    end
+
+    open(FeedFile, 'w') { |f| f.write(feed.to_xml) }
+  end
 end
